9cb646bb73ccaf067a029d642f6c057740e20615
Vickyitsme <105181080+Vickyitsme@users.noreply.github.com>
2025-02-20 13:41:48 +0000

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,13 @@
+import React from 'react';
+   import VideoChat from './VideoChat';
 
+   function App() {
+       return (
+           <div>
+               <h1>PEAPLE Video Chat</h1>
+               <VideoChat />
+           </div>
+       );
+   }
+
+   export default App;
